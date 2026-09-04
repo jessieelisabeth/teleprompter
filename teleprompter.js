0615b787d9f8990809e5eb4849c866868e836963
Jess <villagranjessie03@gmail.com>
2026-09-03 18:07:40 -0600

    added autoscroll

--- teleprompter.js
+++ teleprompter.js
@@ -1,3 +1,11 @@
 const teleprompterText = sessionStorage.getItem("teleprompterText");
 const display = document.getElementById("teleprompter-text-display");
 display.textContent = teleprompterText;
+
+function autoScroll() {
+    container.scrollTop += 1;
+}
+
+const container = document.getElementById("teleprompter-container");
+setInterval(autoScroll, 20);
+
